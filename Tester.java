a051f481c9bbb5351b2d9f0fb904317b255ddd26
lauk20 <klau20@stuy.edu>
2021-01-06 12:05:05 -0500

    Added Node constructor and get methods.

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester{
+  public static void main(String[] args){
+    Node A = new Node("1");
+    Node B = new Node("2");
+    Node C = new Node("3");
+    System.out.println(A.getData());
+    System.out.println(B.getData());
+    System.out.println(C.getData());
+  }
+}
